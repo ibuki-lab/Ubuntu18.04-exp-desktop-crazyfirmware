786cea164cbab2a487eaabe80a9355616762d5e2
ibuki-lab <ibukilab1@gmail.com>
2022-08-19 12:10:03 +0900

    tune attitude pid Gain

--- src/modules/interface/pid.h
+++ src/modules/interface/pid.h
@@ -30,34 +30,34 @@
 #include <stdbool.h>
 #include "filter.h"
 
-#define PID_ROLL_RATE_KP  250.0
-#define PID_ROLL_RATE_KI  500.0
-#define PID_ROLL_RATE_KD  2.5
+#define PID_ROLL_RATE_KP  50 //250.0
+#define PID_ROLL_RATE_KI  10  //500.0
+#define PID_ROLL_RATE_KD  0  //2.5
 #define PID_ROLL_RATE_INTEGRATION_LIMIT    33.3
 
-#define PID_PITCH_RATE_KP  250.0
-#define PID_PITCH_RATE_KI  500.0
-#define PID_PITCH_RATE_KD  2.5
+#define PID_PITCH_RATE_KP  50 //250.0
+#define PID_PITCH_RATE_KI  10  //500.0
+#define PID_PITCH_RATE_KD  0  //2.5
 #define PID_PITCH_RATE_INTEGRATION_LIMIT   33.3
 
-#define PID_YAW_RATE_KP  120.0
-#define PID_YAW_RATE_KI  16.7
-#define PID_YAW_RATE_KD  0.0
+#define PID_YAW_RATE_KP  30 //120.0
+#define PID_YAW_RATE_KI  7  //16.7
+#define PID_YAW_RATE_KD  0  //0.0
 #define PID_YAW_RATE_INTEGRATION_LIMIT     166.7
 
-#define PID_ROLL_KP  6.0
-#define PID_ROLL_KI  3.0
-#define PID_ROLL_KD  0.0
+#define PID_ROLL_KP  10 //6.0
+#define PID_ROLL_KI  5 //3.0
+#define PID_ROLL_KD  0 //0.0
 #define PID_ROLL_INTEGRATION_LIMIT    20.0
 
-#define PID_PITCH_KP  6.0
-#define PID_PITCH_KI  3.0
-#define PID_PITCH_KD  0.0
+#define PID_PITCH_KP  10 //6.0
+#define PID_PITCH_KI  5  //3.0
+#define PID_PITCH_KD  0  //0.0
 #define PID_PITCH_INTEGRATION_LIMIT   20.0
 
-#define PID_YAW_KP  6.0
-#define PID_YAW_KI  1.0
-#define PID_YAW_KD  0.35
+#define PID_YAW_KP  10     //6.0
+#define PID_YAW_KI  0     //1.0
+#define PID_YAW_KD  0.35  //0.35
 #define PID_YAW_INTEGRATION_LIMIT     360.0
 
 
